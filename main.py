8113908704f54f5eabe63fd4b281eb77b5f94208
datta07 <akula.gurudatta@gmail.com>
2023-05-08 00:29:53 +0530

    added some code to restrict for lazy loads

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 from fastapi import FastAPI
+import time
 
 app=FastAPI()
 session=requests.session()
 
 @app.get("/getLink")
-def getMkVlink(id):
+def getMkVlink(id,max_tries=10):
 	res=session.get("https://pdisk.pro/"+id)
 	soup=BeautifulSoup(res.text,"lxml")
 	try:
@@ -14,7 +15,13 @@ def getMkVlink(id):
 		"status":"success",
 		"data":soup.findAll("img",{"class":"vanb"})[-1]["onclick"].split("intent://")[1].split("#Intent")[0]}
 	except Exception:
-		return {
-		"status":"failure",
-		"data":"please contact datta, there is some error"}
+		if (max_tries>0):
+			print("Iterating one more time")
+			time.sleep(1)
+			return getMkVlink(id,max_tries-1)
+		else:
+			return {
+				"status":"failure",
+				"data":"please contact datta, there is some error"}
+
 
