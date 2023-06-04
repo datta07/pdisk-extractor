266431fb20e65f3b5a529aea65c919dedd899ef7
datta07 <akula.gurudatta@gmail.com>
2023-06-04 17:06:22 +0530

    added cors

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import time
 
 app=FastAPI()
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 session=requests.session()
 
 @app.get("/getLink")
